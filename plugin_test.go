a3d23fae6bc48b4cf758e2961d47e3f3225f476e
Adrian Todorov <adrian.ttodorov@gmail.com>
2020-02-17 22:01:10 +0100

    start tests

---
+++ plugin_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func buildEnv() {
+}
+
+func TestParse(t *testing.T) {
+	//	testConf :=
+	args = os.Args[o:1]
+	args = append
+}
+
+func TestShouldSetProject(t *testing.T) {
+}
+
+func TestGetEnvironment(t *testing.T) {
+}
+
+func TestBuildUse(t *testing.T) {
+}
+
+func TestBuildDeploy(t *testing.T) {
+}
+
+func TestExecute(t *testing.T) {
+}
